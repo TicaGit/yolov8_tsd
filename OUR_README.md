fc35f2043e0c47d55fad93855522ef1345148a24
Thibaud Paco Trinca <trinca@izar.ib0.cm.izar.local>
2023-05-26 10:13:39 +0200

    metrics and inference done

---
+++ OUR_README.md
@@ -0,0 +1,71 @@
+add all raw models are in the trained_models folder
+download from https://github.com/ultralytics/assets/releases : take n,m,x
+
+all trained model on drive : put in our_retrained_models
+
+our retrained models on the BTSD dataset are in the folder our_retrained_models
+
+test that everything works on a new environment (load the right files, and not the ultralytics librairy)
+
+data in /work/vita/...
+
+use val function of yolo for evaluate performanace of models 
+
+if other data loc, change path in streamloader.py : data_dir = "/work/vita/nmuenger_trinca/annotations/"
+
+
+# YODSO : YOU ONLY DETECT SIGN ONCE
+
+### instalation:
+
+```
+pip instal ...
+ultralytics
+```
+
+
+### data:
+
+path in .yalm + hardcoded in stream_loader
+
+data are in scitas
+
+say wich sign corespond to which class name
+
+### models:
+
+Before everything, one must first download the models from google drive and put them in the correct files.  
+There are two kinds of models. The models pretrained by the ultralytics team on the coco dataset and our models which we finetuned on the BelgiumTS dataset. Both kinds comes with 3 different size : nano, medium and x-tra large. 
+
+The pretrained models must first be trained with our training script. Our finetuned models can directly be used for inference. 
+
+The pretrained models form ultralytics must be placed in the *trained_models* folder and our finetuned models in the *our_retrained_models* folder.
+
+
+
+### training:
+
+The script to train the network is called our_train.py. One can run it with this command, specifying the model to use and the number of epochs.
+```
+python our_train.py --model yolov8m.pt --epochs 3
+```
+The model trained and some training metrics are then stored in the *run/detect* folder.
+
+### inference:
+
+To infer the bounding boxes and classes of an image, we use the *our_inference.py* script. One can specify the model to use, the path to the image (wich must be in the same folder as the path specified in the .yaml file) and the file to outpur the predictions. An example command is provided below.
+
+```
+python our_inference.py --model_path yolov8m_tsd_30epochs.pt --data_path images/01/image.006950.jp2 --output_file our_predictions/prediction.txt
+```
+
+### testing:
+
+change in the yaml file
+
+
+
+
+
+
+
--- our_metrics_test.py
+++ our_metrics_test.py
@@ -5,10 +5,11 @@ from test_yolo_data_cluster import YOLOCustom
 
 if __name__ == "__main__":
 
+    print("evaluating metrics")
+
     all_test_set = True
     if all_test_set:
-        # CHANGE val:val.txt to test.txt in the yalm file !!!!!
-        test_data_file = "tsr_dataset.yaml"  #rest of the path is hardcoded in stream_loader.py
+        test_data_file = "tsr_dataset_test.yaml"  #rest of the path is hardcoded in stream_loader.py
 
 
         maps50 = []
@@ -16,7 +17,7 @@ if __name__ == "__main__":
         time = []
 
         #NANO
-        model_path = "our_retrained_models/yolov8n_tsd_30epochs.pt"
+        model_path = "our_retrained_models/yolov8n_tsd.pt"
         model = YOLOCustom(model = model_path)
         metrics = model.val(data = test_data_file)
         print(f"Final results for yolov8 nano: mAP50 {metrics.results_dict['metrics/mAP50(B)']}, "+
@@ -24,10 +25,10 @@ if __name__ == "__main__":
         maps50.append(metrics.results_dict['metrics/mAP50(B)'])
         maps50_95.append(metrics.results_dict['metrics/mAP50-95(B)'])
         time.append(metrics.speed['inference'])
-        # Final results for yolov8 nano: mAP50 0.8417955264534036, mAP50 0.7083651463474747 and inference time 2.656630054692028
+        # Final results for yolov8 nano: mAP50 0.4432601368850605, mAP50 0.35808306560387837 and inference time 2.4061742004992466
 
         #MEDIUM
-        model_path = "our_retrained_models/yolov8m_tsd_30epochs.pt"
+        model_path = "our_retrained_models/yolov8m_tsd.pt"
         model = YOLOCustom(model = model_path)
         metrics = model.val(data = test_data_file)
         print(f"Final results for yolov8 medium: mAP50 {metrics.results_dict['metrics/mAP50(B)']}, "+
@@ -35,10 +36,10 @@ if __name__ == "__main__":
         maps50.append(metrics.results_dict['metrics/mAP50(B)'])
         maps50_95.append(metrics.results_dict['metrics/mAP50-95(B)'])
         time.append(metrics.speed['inference'])
-        # Final results for yolov8 medium: mAP50 0.8988986169684553, mAP50 0.8020364596757812 and inference time 5.640034697708059
+        # Final results for yolov8 medium: mAP50 0.524872974614051, mAP50 0.4547125627884293 and inference time 5.599455987948984
         
         #LARGE
-        model_path = "our_retrained_models/yolov8x_tsd_30epochs.pt"
+        model_path = "our_retrained_models/yolov8x_tsd.pt"
         model = YOLOCustom(model = model_path)
         metrics = model.val(data = test_data_file)
         print(f"Final results for yolov8 x-tra large: mAP50 {metrics.results_dict['metrics/mAP50(B)']}, "+
@@ -46,7 +47,7 @@ if __name__ == "__main__":
         maps50.append(metrics.results_dict['metrics/mAP50(B)'])
         maps50_95.append(metrics.results_dict['metrics/mAP50-95(B)'])
         time.append(metrics.speed['inference'])
-        # Final results for yolov8 x-tra large: mAP50 0.9343034899392261, mAP50 0.8510913090950882 and inference time 13.632428332863856
+        # Final results for yolov8 x-tra large: mAP50 0.5595280276367262, mAP50 0.48758862706402817 and inference time 13.416062195496893
 
         plt.plot(time, maps50, label = "maps50")
         plt.plot(time, maps50_95, label = "maps50_95")
@@ -69,27 +70,27 @@ if __name__ == "__main__":
         plt.savefig("our_predictions/random.png")
     
     else:
-        test_image = "fafaa.txt"  #rest of the path is hardcoded in stream_loader.py
+        test_image = "tsr_dataset_one_image.yaml"  #rest of the path is hardcoded in stream_loader.py
 
         print("here")
         time = []
 
         #NANO
-        model_path = "our_retrained_models/yolov8n_tsd_30epochs.pt"
+        model_path = "our_retrained_models/yolov8n_tsd.pt"
         model = YOLOCustom(model = model_path)
         metrics = model.val(data = test_image)
         print(f"Final inference time for yolov8 nano {metrics.speed['inference']}")
         time.append(metrics.speed['inference'])
 
         #NANO
-        model_path = "our_retrained_models/yolov8m_tsd_30epochs.pt"
+        model_path = "our_retrained_models/yolov8m_tsd.pt"
         model = YOLOCustom(model = model_path)
         metrics = model.val(data = test_image)
         print(f"Final inference time for yolov8 medium {metrics.speed['inference']}")
         time.append(metrics.speed['inference'])
 
         #NANO
-        model_path = "our_retrained_models/yolov8x_tsd_30epochs.pt"
+        model_path = "our_retrained_models/yolov8x_tsd.pt"
         model = YOLOCustom(model = model_path)
         metrics = model.val(data = test_image)
         print(f"Final inference time for yolov8 x-tra large {metrics.speed['inference']}")
